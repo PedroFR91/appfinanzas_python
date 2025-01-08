5d2acff0c8721e2aacf338fda1baf66db9489361
PedroFR91 <pedromfr91@gmail.com>
2025-01-08 18:38:03 +0100

    add requirements.txt

--- app.py
+++ app.py
@@ -181,5 +181,6 @@ def upload_file():
         print(f"Error al procesar el archivo: {e}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
